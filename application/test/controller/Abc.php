7871133f692c162061484e5d7a6df9dc349e00dd
WY1126 <1770624149@qq.com>
2022-01-01 16:09:39 +0800

    001

---
+++ application/test/controller/Abc.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\test\controller;
+class Abc
+{
+    public function eat($who = '隔壁老王')
+    {
+        $data = array(
+            'a'     =>      54,
+            'b'     =>      25,
+            'c'     =>      $who,
+        );
+        return json($data);
+    }
+
+}
